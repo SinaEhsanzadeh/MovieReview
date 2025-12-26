371fafd82b0ed99ab977dad9ded427bf6a937b55
Sina Ehsanzadeh <s.eh.81088135@gmail.com>
2025-12-26 18:02:19 +0330

    fix: make changes to database session

--- app/db/session.py
+++ app/db/session.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
 engine = create_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
