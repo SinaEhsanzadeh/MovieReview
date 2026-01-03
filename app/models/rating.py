4f109cb413d4dff6bd3e3786caba993d2bcb98e3
Sam Ghorbani <Sam.ghorbani8490@gmail.com>
2026-01-03 23:07:54 +0330

    feat: add created_at column to MovieRating class

--- app/models/rating.py
+++ app/models/rating.py
@@ -1,5 +1,7 @@
-from sqlalchemy import ForeignKey
+from datetime import datetime
+from sqlalchemy import ForeignKey, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql import func
 
 from app.db.base import Base
 
@@ -11,6 +13,7 @@ class MovieRating(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id", ondelete="CASCADE"), nullable=False)
     score: Mapped[int] = mapped_column(nullable=False) #Should be between 1 and 10
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
 
     #one-to-many relationship between movies and ratings
     movie: Mapped["Movie"] = relationship("Movie", back_populates="ratings")
