7fa33b9a0581d1f26d53acabdf1edbcf6f544192
Sam Ghorbani <Sam.ghorbani8490@gmail.com>
2026-01-03 23:21:18 +0330

    refactor: add more type hints into service layer

--- app/services/movie_service.py
+++ app/services/movie_service.py
@@ -14,19 +14,18 @@ class MovieService:
         return {"page": page, "page_size": page_size, "total_items": total, "items": items} 
 
 
-    def get_movie(self, movie_id: int):
+    def get_movie(self, movie_id: int) -> Movie:
         m = self.repo.get_by_id(movie_id)
         if not m:
             raise NotFoundError("Movie not found")
         return m
 
 
-    def create_movie(self, payload: dict):
+    def create_movie(self, payload: dict) -> Movie:
         # validate director
         director = self.repo._get_director(payload["director_id"])
         if not director:
             raise ValidationError("Invalid director_id")
-
 
         # validate genres
         if payload.get("genres"):
@@ -35,15 +34,13 @@ class MovieService:
             if found_count != len(payload["genres"]):
                 raise ValidationError("One or more genre ids are invalid")
 
-
         movie = self.repo.create(payload["title"], payload["director_id"], payload.get("release_year"), payload.get("cast"))
         if payload.get("genres"):
             self.repo.add_genres(movie, payload["genres"])
-
         return movie
 
 
-    def add_rating(self, movie_id: int, score: int):
+    def add_rating(self, movie_id: int, score: int) -> MovieRating:
         movie = self.get_movie(movie_id)
         if not movie:
             raise NotFoundError("Movie not found")
